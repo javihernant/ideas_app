9cb8e5936a54fc97620d14a0002d98ab03b1b345
Javier Hernandez <javihernant@gmail.com>
2025-02-01 23:34:31 +0100

    feat(users): add ability to list and accept requests
    allow user to list their follow requests.
    allow user to accept user request received
    allow user to deny user request received

--- ideas_app/users/root.py
+++ ideas_app/users/root.py
@@ -1,17 +1,28 @@
 import strawberry
-from strawberry import Info
+from strawberry import Info, relay
 import strawberry_django
 from gqlauth.user.queries import UserQueries, UserType
 from gqlauth.user import arg_mutations as mutations
 from django.core.exceptions import PermissionDenied, ValidationError
 from .models import UserConnection, CustomUser
 from .types import UserConnectionType
-from typing import cast
+from typing import cast, Iterable
+from strawberry_django.relay import ListConnectionWithTotalCount
 
 
 @strawberry.type
 class UsersQuery:
     me: UserType = UserQueries.me
+
+    @strawberry_django.connection(
+        ListConnectionWithTotalCount[UserConnectionType],
+    )
+    def received_requests(self, info: Info) -> Iterable[UserConnection]:
+        user = info.context["request"].user
+        if not user or not user.is_authenticated or not user.is_active:
+            raise PermissionDenied("No user logged in")
+        requests = user.followers.filter(is_accepted=False)
+        return requests
 
 
 @strawberry.type
@@ -44,3 +55,41 @@ class UsersMutation:
         new_conn = UserConnection(followed=follow_user, follower=user)
         new_conn.save()
         return cast(UserConnectionType, new_conn)
+
+    @strawberry_django.mutation(handle_django_errors=True)
+    def accept_request(
+        self, info: Info, user_connection: relay.GlobalID
+    ) -> UserConnectionType:
+        user = info.context["request"].user
+
+        if not user or not user.is_authenticated or not user.is_active:
+            raise PermissionDenied("No user logged in")
+
+        connection = user_connection.resolve_node_sync(info, ensure_type=UserConnection)
+        if connection.followed != user:
+            raise PermissionDenied("User has no permission to accept specified request")
+
+        connection.is_accepted = True
+        connection.save()
+        return connection
+
+    @strawberry_django.mutation(handle_django_errors=True)
+    def deny_request(
+        self, info: Info, user_connection: relay.GlobalID
+    ) -> UserConnectionType:
+        user = info.context["request"].user
+
+        if not user or not user.is_authenticated or not user.is_active:
+            raise PermissionDenied("No user logged in")
+
+        connection = user_connection.resolve_node_sync(info, ensure_type=UserConnection)
+        if connection.followed != user:
+            raise PermissionDenied("User has no permission to deny specified request")
+
+        if connection.is_accepted:
+            raise PermissionDenied("Could not deny request. It was already accepted")
+
+        conn_pk = connection.pk
+        connection.delete()
+        connection.pk = conn_pk
+        return connection
